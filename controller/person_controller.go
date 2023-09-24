c2d615ce825cc32a406e4ceb54daec27063e67dd
KIRIL <kirilpoposki@hotmail.com>
2023-09-24 18:52:00 +0200

    more users to be stored in controller

--- controller/person_controller.go
+++ controller/person_controller.go
@@ -8,20 +8,24 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-var person *model.Person = nil
+var persons []*model.Person = nil
 
 func SetPerson(c *fiber.Ctx) error {
-	person = new(model.Person)
-	if err := c.BodyParser(person); err != nil {
+	temp_person := new(model.Person)
+	if err := c.BodyParser(temp_person); err != nil {
 		fmt.Print(err.Error())
 		return err
 	}
+	persons = append(persons, temp_person)
 	return c.Status(fiber.StatusOK).SendString("Person created")
 }
 
 func GetPerson(c *fiber.Ctx) error {
-	if person != nil {
-		person_string := model.GetName(person) + " " + model.GetSurname(person)
+	if persons != nil {
+		person_string := ""
+		for _, person := range persons {
+			person_string = person_string + model.GetName(person) + " " + model.GetSurname(person) + "\n"
+		}
 		return c.Status(fiber.StatusOK).SendString(person_string)
 	}
 	return c.Status(fiber.StatusOK).SendString("No person created")
